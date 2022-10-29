70434e609d4d50e9ebf421fc823fd0a461b73338
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2022-10-29 23:28:48 +0200

    Updated tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,30 @@
 // The next line exists to trick play.rust-lang.org into running our code as a
 // test:
 // fn main
-#[cfg(test)]
-mod tests {}
-
 mod messages;
 #[cfg(test)]
+mod tests {
+    use crate::messages;
 
-/// Checks if the tweet url is transformed correctly.
-#[test]
-fn test_vxtwitter_1() {
-    let message = messages::Message {
-        content: String::from("https://twitter.com/timClicks/status/1586130960151900160"),
-    };
-    let result = message.vx_twitter();
-    assert_eq!(
-        result,
-        "https://vxtwitter.com/timClicks/status/1586130960151900160"
-    );
-}
-/// Checks if a normal tweet user is ignored correctly.
-#[test]
-fn test_vxtwitter_2() {
-    let message = messages::Message {
-        content: String::from("https://twitter.com/timClicks"),
-    };
-    let result = message.vx_twitter();
-    assert_eq!(result, "");
+    /// Checks if the tweet url is transformed correctly.
+    #[test]
+    fn test_vxtwitter_1() {
+        let message = messages::Message {
+            content: String::from("https://twitter.com/timClicks/status/1586130960151900160"),
+        };
+        let result = message.vx_twitter();
+        assert_eq!(
+            result,
+            "https://vxtwitter.com/timClicks/status/1586130960151900160"
+        );
+    }
+    /// Checks if a normal tweet user is ignored correctly.
+    #[test]
+    fn test_vxtwitter_2() {
+        let message = messages::Message {
+            content: String::from("https://twitter.com/timClicks"),
+        };
+        let result = message.vx_twitter();
+        assert_eq!(result, "");
+    }
 }
