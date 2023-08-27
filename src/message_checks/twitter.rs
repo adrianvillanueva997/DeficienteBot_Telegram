a002dcaa30e3094a7b29175e3a77c437b6f5637f
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2023-08-27 21:08:19 +0200

    removed non necessary print

--- src/message_checks/twitter.rs
+++ src/message_checks/twitter.rs
@@ -1,6 +1,5 @@
 /// Checks if the message contains a twitter link and if it does, it replaces it with a link to the same tweet but in fx.
 pub async fn update_vxtwitter(message: &str) -> Option<String> {
-    println!("{}", message);
     if (message.contains("twitter") && message.contains("status"))
         && !(message.contains("fxtwitter") || message.contains("vxtwitter"))
     {
