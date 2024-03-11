ee94b16b05c183bf99fb007e60935eadd70db508
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2024-03-11 11:45:38 +0100

    updated deprecated function

--- src/ranking/rank.rs
+++ src/ranking/rank.rs
@@ -15,7 +15,11 @@ impl Rank {
 
     #[instrument]
     async fn set_rank(&self, command_id: &str, score: i32) {
-        let mut connection = self.redis_client.get_async_connection().await.unwrap();
+        let mut connection = self
+            .redis_client
+            .get_multiplexed_async_connection()
+            .await
+            .unwrap();
         let _: () = connection
             .zadd(LEADERBOARD, command_id, score)
             .await
@@ -23,13 +27,21 @@ impl Rank {
     }
     #[instrument]
     async fn get_score(&self, command_id: &str) -> Option<i32> {
-        let mut connection = self.redis_client.get_async_connection().await.unwrap();
+        let mut connection = self
+            .redis_client
+            .get_multiplexed_async_connection()
+            .await
+            .unwrap();
         let score: Option<i32> = connection.zscore(LEADERBOARD, command_id).await.unwrap();
         score
     }
     #[instrument]
     pub async fn get_ranking(&self) -> Result<Vec<(String, i32)>, redis::RedisError> {
-        let mut connection = self.redis_client.get_async_connection().await.unwrap();
+        let mut connection = self
+            .redis_client
+            .get_multiplexed_async_connection()
+            .await
+            .unwrap();
         let mut cmd = redis::cmd("ZRANGE");
         cmd.arg(LEADERBOARD).arg(0).arg(-1).arg("WITHSCORES");
         let mut ranking: Vec<(String, i32)> = cmd.query_async(&mut connection).await?;
