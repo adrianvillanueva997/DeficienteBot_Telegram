704892bbf2ab19eb2fb5c0ec69f910dd26c24aab
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2024-07-28 13:32:51 +0200

    fix tnktok bug

--- src/message_checks/tiktok.rs
+++ src/message_checks/tiktok.rs
@@ -1,25 +1,19 @@
-use lazy_static::lazy_static;
-use regex::Regex;
-use tracing::instrument; // Add import for lazy_static
+use tracing::instrument;
 const TNTOK: &str = "tnktok";
 
-lazy_static! {
-    static ref RE: Regex = Regex::new(r"https://(www\.|vm\.)?tiktok\.com/.*").unwrap();
-}
-
-/// Checks if the message contains a tiktok link and if it does, it replaces it with a link to tntok (a privacy focused version of tiktok)
+/// Checks if the message contains a tiktok url and if it does, it replaces "tiktok" with "tnktok"
 #[instrument]
 pub async fn updated_tiktok(message: &str) -> Option<String> {
-    if RE.is_match(message) {
-        let updated_message = RE.replace_all(message, TNTOK);
-        return Some(updated_message.into_owned());
+    if message.contains("tiktok") {
+        let updated_message = message.replace("tiktok", TNTOK);
+        return Some(updated_message);
     }
     None
 }
 
-/// .
+/// Checks if the message contains the word "tiktok"
 #[must_use]
 #[allow(clippy::module_name_repetitions)]
 pub fn check_if_tiktok(message: &str) -> bool {
-    RE.is_match(message)
+    message.contains("tiktok")
 }
--- tests/test_tiktok.rs
+++ tests/test_tiktok.rs
@@ -3,45 +3,21 @@ mod tests {
     use deficiente_telegram_bot::message_checks::tiktok::{check_if_tiktok, updated_tiktok};
 
     #[tokio::test]
-    async fn test_check_if_tiktok_with_tiktok_link() {
-        let message =
-            "Check out this tiktok link: https://www.tiktok.com/@username/video/123456789";
+    async fn test_updated_tiktok() {
+        let message = "Check out this link: https://www.tiktok.com";
+        let expected = "Check out this link: https://www.tnktok.com";
+        assert_eq!(updated_tiktok(message).await, Some(expected.to_string()));
+    }
+
+    #[test]
+    fn test_check_if_tiktok() {
+        let message = "Check out this link: https://www.tiktok.com";
         assert!(check_if_tiktok(message));
     }
 
-    #[tokio::test]
-    async fn test_check_if_tiktok_without_tiktok_link() {
-        let message = "This is a regular message without any tiktok link";
+    #[test]
+    fn test_check_if_tiktok_false() {
+        let message = "Check out this link: https://www.example.com";
         assert!(!check_if_tiktok(message));
-    }
-
-    #[tokio::test]
-    async fn test_updated_tiktok_with_tiktok_link() {
-        let message =
-            "Check out this tiktok link: https://www.tiktok.com/@username/video/123456789";
-        let expected_result = Some("Check out this tiktok link: tnktok".to_string());
-        assert_eq!(updated_tiktok(message).await, expected_result);
-    }
-
-    #[tokio::test]
-    async fn test_updated_tiktok_without_tiktok_link() {
-        let message = "This is a regular message without any tiktok link";
-        let expected_result = None;
-        assert_eq!(updated_tiktok(message).await, expected_result);
-    }
-
-    // Additional tests
-
-    #[tokio::test]
-    async fn test_check_if_tiktok_with_vm_tiktok_link() {
-        let message = "Check out this tiktok link: https://vm.tiktok.com/@username/video/123456789";
-        assert!(check_if_tiktok(message));
-    }
-
-    #[tokio::test]
-    async fn test_updated_tiktok_with_vm_tiktok_link() {
-        let message = "Check out this tiktok link: https://vm.tiktok.com/@username/video/123456789";
-        let expected_result = Some("Check out this tiktok link: tnktok".to_string());
-        assert_eq!(updated_tiktok(message).await, expected_result);
     }
 }
