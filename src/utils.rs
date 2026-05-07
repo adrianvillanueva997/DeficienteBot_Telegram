c24388f045087878a4cc3823b7653051c27d13c8
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2026-05-08 01:11:38 +0900

    remove unused functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,24 +1,5 @@
 use teloxide::types::Message;
 
-pub fn escape_markdown(text: &str) -> String {
-    let special_chars = [
-        '_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!',
-    ];
-    let mut result = String::with_capacity(text.len() * 2);
-    for c in text.chars() {
-        if special_chars.contains(&c) {
-            result.push('\\');
-        }
-        result.push(c);
-    }
-    result
-}
-
-pub fn get_telegram_username(msg: &Message) -> String {
-    let user = msg.from.as_ref().unwrap().username.as_ref().unwrap();
-    format!("@{user}")
-}
-
 pub fn format_message_username(msg: &Message, content: &str) -> String {
     let username = msg
         .from
